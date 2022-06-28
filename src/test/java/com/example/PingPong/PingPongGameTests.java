a384b894915bd4a644871313963625887d969eae
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-28 17:55:05 +0300

    Addded new test for createing paddles

--- src/test/java/com/example/PingPong/PingPongGameTests.java
+++ src/test/java/com/example/PingPong/PingPongGameTests.java
@@ -3,9 +3,16 @@ package com.example.PingPong;
 import org.junit.jupiter.api.Test;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 class PingPongGameTests {
 
+	static final int GAME_WIDTH = 1000;
+	static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+	static final int PADDLE_WIDTH = 25;
+	static final int PADDLE_HEIGHT = 100;
+	PaddleTest paddleOne;
+	PaddleTest paddleTwo;
 	GamePanel panel;
 
 	@Test
@@ -22,5 +29,26 @@ class PingPongGameTests {
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
 
+	}
+
+	@Test
+	void createPaddles() {
+
+		paddleOne = new PaddleTest(0,(GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2),
+				PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+		paddleTwo = new PaddleTest(GAME_WIDTH - PADDLE_WIDTH, (GAME_WIDTH / 2) - (PADDLE_HEIGHT / 2),
+				PADDLE_WIDTH, PADDLE_HEIGHT, 2);
+
+	}
+
+	public class PaddleTest extends Rectangle {
+
+		int id;
+		int yVelocity;
+
+		PaddleTest(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
+			super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+			this.id = id;
+		}
 	}
 }
