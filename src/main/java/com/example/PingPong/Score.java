9885869cf2ec907d2783a01155ee67cdaab0e040
Nikola <nikola.malinov.krastev@gmail.com>
2022-06-29 10:52:24 +0300

    Added the scoreboard on the screen of the game

--- src/main/java/com/example/PingPong/Score.java
+++ src/main/java/com/example/PingPong/Score.java
@@ -22,5 +22,8 @@ public class Score extends Rectangle {
         g.setFont(new Font("Helvetica", Font.PLAIN, 60));
 
         g.drawLine(GAME_WIDTH / 2, 0, GAME_WIDTH / 2, GAME_HEIGHT);
+
+        g.drawString(String.valueOf(playerOne / 10) + String.valueOf(playerOne % 10), (GAME_WIDTH / 2) - 85, 50);
+        g.drawString(String.valueOf(playerTwo / 10) + String.valueOf(playerTwo % 10), (GAME_WIDTH / 2) + 20, 50);
     }
 }
